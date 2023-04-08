3fe80cbdf7d5ef8df4b586f177cd2b1924f07048
Rubeevatshr08 <126511661+Rubeevatshr08@users.noreply.github.com>
2023-04-08 11:49:01 +0530

    Update Main.java

--- MemberVariableDeclaration/Main.java
+++ MemberVariableDeclaration/Main.java
@@ -1,9 +1,20 @@
 package MemberVariableDeclaration;
 
+class Car
+{
+    float speed;
+    float distanceCovered;
+
+}
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(c);
+        Car thar = new Car();
+        thar.speed = 200;
+        thar.distanceCovered = 0;
+        System.out.println(thar.speed);
+        System.out.println(thar.distanceCovered);
     }
 }
