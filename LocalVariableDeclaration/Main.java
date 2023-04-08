d6657ad2c3d45612430e8b666fdf75dfd9947e39
Rubeevatshr08 <126511661+Rubeevatshr08@users.noreply.github.com>
2023-04-08 11:37:30 +0530

    Update Main.java

--- LocalVariableDeclaration/Main.java
+++ LocalVariableDeclaration/Main.java
@@ -2,6 +2,9 @@ package LocalVariableDeclaration;
 
 public class Main {
     public static void main( String[] args ) {
-        
+        int a = 10;
+        int b = 20;
+        float c = b/a;
+        System.out.println(c);
     }
 }
